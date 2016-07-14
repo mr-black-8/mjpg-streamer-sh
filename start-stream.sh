31eb959f529d0ddf9b412530d7dacf38a5147822
mr-black-8 <boz.black@gmail.com>
2016-07-15 01:55:52 +1000

    updated

--- start-stream.sh
+++ start-stream.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-if [ ! -d /tmp/stream ]
-then
-    mkdir /tmp/stream/
-fi
-
-if pgrep raspistill > /dev/null
-then
-    echo "raspistill already running"
-else
-    raspistill -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 -n > /dev/null 2>&1&
-    echo "raspistill started"
-fi
-
 if pgrep mjpg_streamer > /dev/null
 then
-    echo "mjpg_streamer already running"
+  echo "mjpg_streamer already running"
 else
-    LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
-    echo "mjpg_streamer started"
+  LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 15 -q 50 -x 640 -y 480" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
+  echo "mjpg_streamer started"
 fi
